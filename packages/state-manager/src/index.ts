d954a609985954b82ad8776a44d0c187711cfa35
Hyeongmin <hmyoo.dev@gmail.com>
2020-11-04 20:20:23 +0900

    Set main point

--- packages/state-manager/src/index.ts
+++ packages/state-manager/src/index.ts
@@ -1,3 +1,5 @@
-export function greeting(name: string): string {
-  return `Hello ${name}`;
-}
+export * from "./actionCreators";
+export * from "./Controller";
+export * from "./Service";
+export * from "./Store";
+export * from "./types";
--- packages/state-manager/test/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { greeting } from "../src/index";
-
-describe("greeting", () => {
-  it("should return hello", () => {
-    expect(greeting("world").toLowerCase()).toContain("hello");
-  });
-});
