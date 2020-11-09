71631e12ebc3858b44b5fef93049a3b54d1ef3b4
Hyeongmin <hmyoo.dev@gmail.com>
2020-11-09 12:25:21 +0900

    Fix examples babel config

--- examples/.babelrc.js
+++ examples/.babelrc.js
@@ -1,7 +1,8 @@
 module.exports = {
   presets: [
+    "@babel/preset-env",
     "@babel/preset-typescript",
-    ["@babel/preset-env", { targets: { node: "current" } }],
     "@babel/preset-react",
   ],
+  plugins: ["@babel/proposal-class-properties"],
 };
