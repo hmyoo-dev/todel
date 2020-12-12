50331f0ce686d5379d25f84a7e1ff69e635b5325
Hyeong-min <hmyoo.dev@gmail.com>
2020-12-12 09:24:15 +0900

    Remove useServiceState hook

--- packages/todel-react/src/index.ts
+++ packages/todel-react/src/index.ts
@@ -2,5 +2,4 @@ export * from "./StoreContext";
 export * from "./StoreProvider";
 export * from "./useAtom";
 export * from "./useDispatch";
-export * from "./useServiceState";
 export * from "./utils";
