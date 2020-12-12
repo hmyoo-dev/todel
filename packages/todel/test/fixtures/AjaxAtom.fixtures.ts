93888c1a7cb8ace57d055bfa161ade24b7a21ef1
Hyeong-min <hmyoo.dev@gmail.com>
2020-12-12 21:24:59 +0900

    Add AjaxAtom

---
+++ packages/todel/test/fixtures/AjaxAtom.fixtures.ts
@@ -0,0 +1,37 @@
+import {
+  AjaxAtom,
+  AjaxAtomState,
+  AjaxStatus,
+  idleAjaxAtomState,
+} from "../../src/AjaxAtom";
+
+export interface Author {
+  name: string;
+}
+
+export type AuthorAtomState = AjaxAtomState<Author | null>;
+
+export class AuthorAtom extends AjaxAtom<Author | null> {
+  static idle(): AuthorAtom {
+    return new AuthorAtom(idleAjaxAtomState(null));
+  }
+
+  static pending(): AuthorAtom {
+    return new AuthorAtom({
+      ...idleAjaxAtomState(null),
+      status: AjaxStatus.Pending,
+    });
+  }
+
+  static done(author: Author): AuthorAtom {
+    return new AuthorAtom({
+      value: author,
+      status: AjaxStatus.Success,
+      error: null,
+    });
+  }
+
+  get defaultValue(): Author | null {
+    return null;
+  }
+}
