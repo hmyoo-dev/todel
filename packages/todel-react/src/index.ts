80fba5f81a6b407946f08ccda6d11ab8f3480acc
Hyeong-min <hmyoo.dev@gmail.com>
2020-12-21 17:31:47 +0900

    fix: Add missing types

--- packages/todel-react/src/index.ts
+++ packages/todel-react/src/index.ts
@@ -2,5 +2,6 @@ export * from "./createDataHook";
 export * from "./createJoinDataHook";
 export * from "./StoreContext";
 export * from "./StoreProvider";
+export * from "./types";
 export * from "./useDispatch";
 export * from "./utils";
