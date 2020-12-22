f11402629fa426100a1bbc7f7d07e578aa4f683b
Hyeong-min <hmyoo.dev@gmail.com>
2020-12-22 22:51:04 +0900

    feat: Export PubSub

--- packages/todel/src/index.ts
+++ packages/todel/src/index.ts
@@ -2,5 +2,6 @@ export * from "./actionCreators";
 export * from "./AjaxAtom";
 export * from "./Atom";
 export * from "./controllerHelpers";
+export * from "./PubSub";
 export * from "./Store";
 export * from "./types";
